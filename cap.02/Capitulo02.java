d57f1005f4db2a541fa4b473a03608a8b42bad2a
WillianFarias <williansousafarias@gmail.com>
2020-09-27 22:16:39 -0300

    Class Consumer

--- cap.02/Capitulo02.java
+++ cap.02/Capitulo02.java
@@ -1,5 +1,17 @@
+import java.util.List;
+import java.util.Arrays;
+
 public class Capitulo02 {
   public static void main(String ... args) {
-    
+    Usuario user1 = new Usuario("Willian Farias", 150);
+    Usuario user2 = new Usuario("Samanta Farias", 120);
+    Usuario user3 = new Usuario("Amanda Alvez", 190);
+
+    //Forma de criar uma lista imutavel
+    List<Usuario> usuarios = Arrays.asList(user1, user2, user3);
+
+    for(Usuario u : usuarios){
+      System.out.println(u.getNome());
+    }
   }
 }
